938f54b3b9c6b662b6f11cc0bcc7e9dd2e9f4900
retha mbombo <rethambombo@gmail.com>
2026-07-14 10:33:21 +0200

    declaration in variable

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-alert("hello word!");
+/*alert("hello word!");
 alert("5 + 6!");
+
 "use strick";
-alert("some code");
+alert("some code");*/
+let message;
+ message = 'Salut';
+
+ alert('Salut');
+ 
+ let message ='Salut';
+
+ alert('Salut');
+
+ let age = 25, user = 'john', message = 'Bonsoir';
+
+ let user ='john';
+ let age= 25 ;
+ let message = 'Bonsoir';
+ let user = 'john',
+     age = 25,
+     message = 'Salut';
