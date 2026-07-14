b96a876e08e9b4fc2d70433d3e948ae36ccb1fe4
retha mbombo <rethambombo@gmail.com>
2026-07-14 17:06:53 +0200

    add a new js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ alert("5 + 6!");
 
 "use strick";
 alert("some code");*/
-let message;
+/*let message;
  message = 'Salut';
 
  alert('Salut');
@@ -20,3 +20,20 @@ let message;
  let user = 'john',
      age = 25,
      message = 'Salut';
+
+let user = 'john'
+   ,age = 25
+   ,message = 'Salut';
+
+alert('john');
+
+let message;
+    message = 'Salut';
+    message = 'Bonsoir';
+
+alert('message');*/
+
+let $ = 1;
+let _ = 6;
+
+alert($ + _);
